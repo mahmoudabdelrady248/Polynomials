03ceeb7f30f6992d798e0165d2dcc89d3c462ddb
M.Abdelrady <M.Abdelrady@192.168.1.4>
2020-03-20 21:48:18 +0200

    Polynomials

--- LinkedList/src/eg/edu/alexu/csd/datastructure/linkedList/DoubleListTest.java
+++ LinkedList/src/eg/edu/alexu/csd/datastructure/linkedList/DoubleListTest.java
@@ -7,28 +7,27 @@ import org.junit.jupiter.api.Test;
 class DoubleListTest {
    @Test
 	void test() {
-		DoubleLinkedList test=new DoubleLinkedList();
-		test.add(1);
-		test.add(2);
-		test.add(3);
-		assertEquals(1,test.get(0));
-		assertEquals(2,test.get(1));
-		assertEquals(3,test.get(2));
+	    DoubleLinkedList test=new DoubleLinkedList();
+		test.add(1);test.add(2);test.add(3);test.add(4);test.add("rr");test.add(4.5);test.add('A');
+		assertEquals(1,test.get(0));assertEquals(2,test.get(1));
+		assertEquals(3,test.get(2));assertEquals(4,test.get(3));
+		assertEquals("rr",test.get(4));assertEquals(4.5,test.get(5));
+		assertEquals('A',test.get(6));
 		assertEquals(null,test.get(50));
 		assertEquals(null,test.get(40));
-		test.add(2, 55);
-		assertEquals(55,test.get(2));
+		test.add(3, 55);
+		assertEquals(55,test.get(3));
 		test.set(0, 1111);
 		assertEquals(1111,test.get(0));
 		assertEquals(null,test.sublist(4,0));
-		Object []sub= {2,55};
-		Object []sublist= {test.get(1),test.get(2)};
+		Object []sub= {2,3,55,4};
+		Object []sublist= {test.get(1),test.get(2),test.get(3),test.get(4)};
 		assertArrayEquals(sub,sublist);
 		test.remove(0);
 		assertEquals(2,test.get(0));
-		assertEquals(3,test.size());
-        assertEquals(true,test.contains(55));
-		assertEquals(false,test.contains(4));
+		assertEquals(7,test.size());
+       assertEquals(true,test.contains(55));
+		assertEquals(false,test.contains(10));
 		test.clear();
 		assertEquals(true,test.isEmpty());
 	}
